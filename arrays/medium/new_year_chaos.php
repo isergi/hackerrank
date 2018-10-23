e6d399fb8a78632cd5a8f57740460c123f578ff9
Sergey Pavlov <s.pavlov@pravo.ru>
2018-10-23 12:20:01 +0300

    arrays new_year_chaos

---
+++ arrays/medium/new_year_chaos.php
@@ -0,0 +1,48 @@
+<?php
+
+// Complete the minimumBribes function below.
+function minimumBribes($q) {
+    $stepsCount = 0;
+    $bribesCounts = [];
+    $arrayLength = sizeOf($q)-1;
+    $swapCntPerRound = 0;
+    for ($i = 0; $i < $arrayLength; $i++) {
+        if ($q[$i] > $q[$i+1]) {
+            if (!isset($bribesCounts[$q[$i]])) {
+                $bribesCounts[$q[$i]] = 0;
+            }
+
+            $bribesCounts[$q[$i]]++;
+
+            if ($bribesCounts[$q[$i]] > 2) {
+                //echo 'Too chaotic', PHP_EOL;
+                return "Too chaotic";
+            }
+
+            $stepsCount++;
+            $swapCntPerRound++;
+            list($q[$i+1], $q[$i]) = [$q[$i], $q[$i+1]];
+        }
+
+        if ($i == $arrayLength-1 && $swapCntPerRound > 0) {
+            $i = -1;
+            $swapCntPerRound = 0;
+        }
+    }
+
+    //echo array_sum($bribesCounts), PHP_EOL;
+    return array_sum($bribesCounts);
+}
+
+echo '<pre>';
+print_r(['[2,1,5,3,4]' => minimumBribes([2,1,5,3,4])]);
+echo '</pre>';
+echo '<pre>';
+print_r(['[2,5,3,1,4]' => minimumBribes([2,5,3,1,4])]);
+echo '</pre>';
+echo '<pre>';
+print_r(['[5,1,2,3,7,8,6,4]' => minimumBribes([5,1,2,3,7,8,6,4])]);
+echo '</pre>';
+echo '<pre>';
+print_r(['[1,2,5,3,7,8,6,4]' => minimumBribes([1,2,5,3,7,8,6,4])]);
+echo '</pre>';
